961b6010460c3e131a5b0b7f24ab879c0e03de44
Victor Longui Pereira <longuivictor@gmail.com>
2023-08-19 03:16:35 -0300

    main & templates

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-from flask import Flask
-
+# chame a biblioteca do Flask
+from flask import Flask, render_template
+# crie uma variável para chamar o Flask, é recomendada q ela seja chamada de app
 app = Flask(__name__)
 
 # criar a 1 pagina do site
-# route (o que vem depois da barra) -> hashtagtreinamentos.com/contatos
+# route (o que vem depois da barra) -> meusite.com/contatos
 # função -> o que voce quer exibir naquela página
 # template
 
 @app.route("/")
 def homepage():
-    return "Esse é o meu primeiro site, a homepage"
+    return render_template("homepage.html")
 
 @app.route("/contatos")
 def contatos():
-    return "<p>Nossos contatos são:</p> <p>email: blablabla@gmail.com</p> <p>telefone: (11)99999-9999</p>"
+    return render_template("contatos.html")
+
+@app.route("/usuarios/<nome_usuario>")
+def usuarios(nome_usuario):
+    return render_template("usuarios.html", nome_usuario=nome_usuario)
+
 
 
 
